71252ae8bd1e9fe40bea1d6e29a89865c020720b
podxboq <podxboq@gmail.com>
2013-05-05 22:14:18 +0200

    Torch mode is out

--- src/com/aokp/romcontrol/fragments/PowerMenu.java
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -13,12 +13,10 @@ import com.aokp.romcontrol.R;
 public class PowerMenu extends AOKPPreferenceFragment {
 
     private static final String PREF_SCREENSHOT = "show_screenshot";
-    private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
     private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
     private static final String PREF_REBOOT_KEYGUARD = "show_reboot_keyguard";
 
     CheckBoxPreference mShowScreenShot;
-    CheckBoxPreference mShowTorchToggle;
     CheckBoxPreference mShowAirplaneToggle;
     CheckBoxPreference mShowRebootKeyguard;
 
@@ -28,10 +26,6 @@ public class PowerMenu extends AOKPPreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_powermenu);
-
-        mShowTorchToggle = (CheckBoxPreference) findPreference(PREF_TORCH_TOGGLE);
-        mShowTorchToggle.setChecked(Settings.System.getBoolean(getActivity()
-                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE, false));
 
         mShowScreenShot = (CheckBoxPreference) findPreference(PREF_SCREENSHOT);
         mShowScreenShot.setChecked(Settings.System.getBoolean(getActivity()
@@ -51,11 +45,6 @@ public class PowerMenu extends AOKPPreferenceFragment {
         if (preference == mShowScreenShot) {
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
-                    ((CheckBoxPreference)preference).isChecked());
-            return true;
-        } else if (preference == mShowTorchToggle) {
-            Settings.System.putBoolean(getActivity().getContentResolver(),
-                    Settings.System.POWER_DIALOG_SHOW_TORCH_TOGGLE,
                     ((CheckBoxPreference)preference).isChecked());
             return true;
         } else if (preference == mShowAirplaneToggle) {
